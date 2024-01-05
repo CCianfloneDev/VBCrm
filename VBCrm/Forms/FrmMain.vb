5225f27cdb067b4f569c13b87bcf52a3c8f5baa6
Cole Cianflone <colecianflone123@gmail.com>
2024-01-04 18:11:36 -0600

    setting up sqlite db
    Using sqlite db, will be created on first form load if not already created

---
+++ VBCrm/Forms/FrmMain.vb
@@ -0,0 +1,21 @@
+﻿''' <summary>
+''' Represents the main form.
+''' </summary>
+Public Class FrmMain
+    Private dbOperations As ClsDbOperations
+
+    ''' <summary>
+    ''' Handles the main form load event.
+    ''' </summary>
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dbOperations = New ClsDbOperations()
+        dbOperations.CreateCustomersTable()
+    End Sub
+
+    ''' <summary>
+    ''' Handles the main form closing event.
+    ''' </summary>
+    Private Sub FrmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        dbOperations.CloseConnection()
+    End Sub
+End Class
--- VBCrm/FrmMain.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class FrmMain
-
-End Class
